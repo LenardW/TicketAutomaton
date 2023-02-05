8110a68e9393b9a9ea491bd6b7b81f92c6a272b6
LenardW <64787608+LenardW@users.noreply.github.com>
2023-02-05 16:16:08 +0100

    Add Component Service Bus TODO: Logger + Eventhandler

---
+++ ComponentServiceBus/src/de/leuphana/cosa/componentservicebus/structure/Component.java
@@ -0,0 +1,31 @@
+package de.leuphana.cosa.componentservicebus.structure;
+
+//import com.google.common.eventbus.EventBus;
+import org.osgi.service.event.;	
+
+public abstract class Component {
+	
+	private EventBus eventBus;
+	
+	public Component() {
+		eventBus = new EventBus();
+	}
+	
+	public abstract String getComponentName();
+	public abstract String getCommandServiceName();
+	public abstract String getEventServiceName();
+	public abstract String getCommandServicePath();
+	public abstract String getEventServicePath();
+	
+	protected void post(Object object) {
+		eventBus.post(object);
+	}
+	
+	protected void register(Object object) {
+		eventBus.register(object);
+	}
+	
+	protected void unregister(Object object) {
+		eventBus.unregister(object);
+	}
+}
